cb515b550caef7c98d40bae86d6556ccb9c6f7e3
KhalidElfaker <KhalidElfaker@users.noreply.github.com>
2018-07-19 19:31:38 +0100

    ListViewAdapter finalization
    adding Picasso the image downloading and caching library for Android, and foramtion the rating number

--- app/src/main/java/com/khalidelfaker/mobilecodingchallenge/MyListViewAdapter.java
+++ app/src/main/java/com/khalidelfaker/mobilecodingchallenge/MyListViewAdapter.java
@@ -5,54 +5,92 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.khalidelfaker.models.GithubRepos;
+import com.khalidelfaker.models.Item;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
 
 public class MyListViewAdapter extends BaseAdapter {
-
     private Context context;
-    ArrayList<?> objects;
-    ArrayList<View> viewArrayList;
+    private List<Item> items;
 
-    public MyListViewAdapter(Context context, ArrayList<?> objects) {
+    public MyListViewAdapter(Context context, GithubRepos repos) {
         this.context = context;
-        this.objects = objects;
+        items = repos.getItems();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        final View cell_list_View;
+        View cell_list_View;
+        TextView repo_name;
+        TextView repo_desc;
+        TextView owner_name;
+        TextView rating;
+        ImageView avatar;
 
-        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        if(convertView==null){
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             cell_list_View = inflater.inflate(R.layout.repos_element_ligne, null);
-            TextView repo_name = cell_list_View.findViewById(R.id.repo_name);
-            repo_name.setText(objects.get(position).toString());
-            notifyDataSetChanged();
-        }
-        else {
+        } else {
             cell_list_View = convertView;
         }
 
+        repo_name = cell_list_View.findViewById(R.id.repo_name);
+        repo_desc = cell_list_View.findViewById(R.id.repos_desc);
+        owner_name = cell_list_View.findViewById(R.id.owner_name);
+        rating = cell_list_View.findViewById(R.id.rating);
+        avatar = cell_list_View.findViewById(R.id.avatar);
+
+        repo_name.setText(items.get(position).getName());
+        repo_desc.setText(items.get(position).getDescription());
+        owner_name.setText(items.get(position).getOwner().getLogin());
+
+        //formatting the number of stars to be shown as expected
+        Long StargazersCount = items.get(position).getStargazersCount();
+        double ratingToShow;
+        String ratingS;
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+        decimalFormatSymbols.setDecimalSeparator('.');
+        DecimalFormat decimalFormat;
+        if (StargazersCount > 999) {
+            ratingToShow = StargazersCount / 1000.0;
+            decimalFormat = new DecimalFormat("0.0K", decimalFormatSymbols);
+        } else if (StargazersCount > 999999) {
+            ratingToShow = StargazersCount / 1000000.0;
+            decimalFormat = new DecimalFormat("0.0M", decimalFormatSymbols);
+        } else {
+            ratingToShow = StargazersCount;
+            decimalFormat = new DecimalFormat("0", decimalFormatSymbols);
+        }
+        ratingS = decimalFormat.format(ratingToShow);
+        rating.setText(ratingS);
+
+        //using Picasso to download profile pictures and manage the caching process
+        String avatarUrl = items.get(position).getOwner().getAvatarUrl();
+        Picasso.get().load(avatarUrl).into(avatar);
+
+        notifyDataSetChanged();
         return cell_list_View;
     }
 
     @Override
     public int getCount() {
-        return objects.size();
+        return items.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return items.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return items.get(position).getId();
     }
-
-
 }
